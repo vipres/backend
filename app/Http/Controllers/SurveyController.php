ba3e79d5989a92c93e22e463c7d409884b9e574d
Manolo Cabrera <viprestal@hotmail.com>
2023-10-26 13:53:19 +0200

    hecha primera migracion Survey

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SurveyStoreRequest;
+use App\Http\Requests\SurveyUdateRequest;
+use App\Models\Survey;
+use Illuminate\Http\Request;
+
+class SurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(SurveyStoreRequest $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Survey $survey)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(SurveyUdateRequest $request, Survey $survey)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Survey $survey)
+    {
+        //
+    }
+}
